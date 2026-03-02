2e6db04e4b19062ea6de4326a70d69f129974683
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-03-02 23:25:44 +0600

    Updated

--- apps/auth-service/bootstrap/app.php
+++ apps/auth-service/bootstrap/app.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +19,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'You must be logged in to access this resource.',
+                    'success' => false,
+                ], 401);
+            }
+        });
     })->create();
