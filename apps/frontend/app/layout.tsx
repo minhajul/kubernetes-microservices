da0a9b39818ce0a8e46a86e3c4ddb97205e680ae
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-03-02 23:08:52 +0600

    Updated

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next";
-import "./globals.css";
 import {Providers} from "./providers";
 import Navbar from "./components/Navbar";
+import "./globals.css";
 
 export const metadata: Metadata = {
     title: "MyApp - Home page",
